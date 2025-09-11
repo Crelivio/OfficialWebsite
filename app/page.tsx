49e633482cfc4235cc12461bf1cf7c801c60c2ec
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-09-11 13:17:30 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <main className='container mx-auto p-4 bg-amber-300 min-h-screen'>
+      <h1>Welcome to the Official Website</h1>
+      <p>This is your homepage. Start building something amazing!</p>
+    </main>
+  );
+}
