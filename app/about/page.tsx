d8cd1510d9d45c3fab753d6b8ac7f99a22a0060e
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-09-18 23:59:56 +0100

    Add About page for Next.js routing

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { BrandSection } from "./sections/brand";
+
+const AboutPage = () => {
+  return (
+    <>
+      <div>
+        {/* Main content of the About page goes here */}
+        <BrandSection />
+      </div>
+    </>
+  );
+};
+
+export default AboutPage;
