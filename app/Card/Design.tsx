7b7578032f4a705fbd80005778a6992105f7a059
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-09-24 21:53:07 +0100

    Set card background to match the deisgn and text color to black in DesignCard

--- app/Card/Design.tsx
+++ app/Card/Design.tsx
@@ -3,10 +3,10 @@ import { Card, CardContent } from "@/components/ui/card";
 
 export const DesignCard = (): React.JSX.Element => {
   return (
-    <Card className="w-full max-w-[200px] sm:max-w-[220px] lg:max-w-[270px] bg-blue-600">
+  <Card className="w-full max-w-[200px] sm:max-w-[220px] lg:max-w-[270px]" style={{ backgroundColor: '#82affa' }}>
       <CardContent className="flex flex-col gap-2 sm:gap-3 p-3 sm:p-4">
         <div className="flex items-center gap-1 sm:gap-2">
-          <h2 className="font-display font-medium text-[#2c2c2c] text-[16px] sm:text-[18px] lg:text-[22px] xl:text-[24px] tracking-[0] leading-[normal]">
+          <h2 className="font-display font-medium text-black text-[16px] sm:text-[18px] lg:text-[22px] xl:text-[24px] tracking-[0] leading-[normal]">
             Design
           </h2>
           <img
@@ -15,7 +15,7 @@ export const DesignCard = (): React.JSX.Element => {
             src="/Icon/design-icon.svg"
           />
         </div>
-        <p className="font-normal text-[#2c2c2c] text-[11px] sm:text-[12px] lg:text-base tracking-[0] leading-[normal]">
+  <p className="font-normal text-black text-[11px] sm:text-[12px] lg:text-base tracking-[0] leading-[normal]">
           Crafting innovative and user-focused designs that blend creativity
           with functionality to deliver impactful visual and digital
           experiences.
