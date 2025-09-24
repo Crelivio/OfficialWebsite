ad8317c33bc31f7166b99a728724e269aaed43de
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-09-24 21:58:34 +0100

    Responsive and style updates in FooterSection

--- app/Section/FooterSection.tsx
+++ app/Section/FooterSection.tsx
@@ -2,20 +2,35 @@ import React from "react";
 
 export const FooterSection = (): React.JSX.Element => {
   return (
-    <footer className="w-full h-44">
-      <div className="flex items-center justify-between h-full px-[105px]">
-        <div className="[font-family:'Bricolage_Grotesque',Helvetica] font-semibold text-[#ffffff] text-xl tracking-[0] leading-[normal] text-left">
+    <footer className="w-full h-auto py-6 sm:py-8 lg:h-44">
+      <div className="flex flex-col sm:flex-row items-center justify-between h-full px-4 sm:px-8 lg:px-20 xl:px-[105px] gap-4 sm:gap-0">
+        <div className="[font-family:'Bricolage_Grotesque',Helvetica] font-semibold text-[#ffffff] text-[14px] sm:text-[16px] lg:text-xl tracking-[0] leading-[normal] text-center sm:text-left">
           © 2025 Crelivio Inc. All rights reserved
         </div>
-        <div className="flex items-center gap-2">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <img className="w-[30px] h-[30px]" alt="Facebook" src="/Icon/facebook.svg" />
+        <div className="flex items-center gap-3 sm:gap-4">
+          <a 
+            href="https://facebook.com" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="hover:opacity-75 transition-opacity duration-200"
+          >
+            <img className="w-[24px] h-[24px] sm:w-[28px] sm:h-[28px] lg:w-[30px] lg:h-[30px]" alt="Facebook" src="/Icon/facebook.svg" />
           </a>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <img className="w-[30px] h-[30px]" alt="Twitter" src="/Icon/twitter.svg" />
+          <a 
+            href="https://twitter.com" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="hover:opacity-75 transition-opacity duration-200"
+          >
+            <img className="w-[24px] h-[24px] sm:w-[28px] sm:h-[28px] lg:w-[30px] lg:h-[30px]" alt="Twitter" src="/Icon/twitter.svg" />
           </a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <img className="w-[30px] h-[30px]" alt="Instagram" src="/Icon/instagram.svg" />
+          <a 
+            href="https://instagram.com" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="hover:opacity-75 transition-opacity duration-200"
+          >
+            <img className="w-[24px] h-[24px] sm:w-[28px] sm:h-[28px] lg:w-[30px] lg:h-[30px]" alt="Instagram" src="/Icon/instagram.svg" />
           </a>
         </div>
       </div>
