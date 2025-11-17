42c8b74d3cd8da0b53fafcab47535845675a1ed9
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-11-17 15:57:09 +0100

    chore: Remove unused imports

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { Button } from "@/components/ui/button";
 import { BrandSection } from "./sections/brand";
 import DiscoverySection from "../Section/DiscoverySection";
 import { FooterSection } from "../Section/FooterSection";
